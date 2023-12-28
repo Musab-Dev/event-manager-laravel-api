e2bad4ba4a801c49e53fee849d058ef8f9459d71
Musab-Dev <musab.dev98@gmail.com>
2023-12-28 14:38:04 +0300

    [database] models added

---
+++ database/migrations/2023_12_28_102901_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->nullable(false);
+            $table->text('description')->nullable();
+            $table->int('capacity')->required();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->unsignedBigInteger('owner_id');
+
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
